56f46281db47081929da802e5e4f0e6f082faf52
X <70487315+XWasHere@users.noreply.github.com>
2021-11-11 12:01:26 -0500

    some useless tests i made in class (3/*)

---
+++ src/tests/util.h
@@ -0,0 +1,43 @@
+#ifndef IRCREBORN_TEST_UTIL_H
+#define IRCREBORN_TEST_UTIL_H
+
+#include <string.h>
+
+#include <common/util.h>
+
+#define TEST_STOP() { \
+    goto __T_STOP__; \
+};
+
+#define TEST_PASS(msg, ...) { \
+    printf(FMT_INFO("test \"%s\" completed normally: " #msg "\n"), __T_NAME__, ## __VA_ARGS__); \
+    TEST_STOP(); \
+};
+
+#define TEST_FAIL(msg, ...) { \
+    printf(FMT_FATL("test \"%s\" failed: " #msg "\n"), __T_NAME__, ## __VA_ARGS__); \
+    TEST_STOP(); \
+};
+
+#define INT_ASSERT_EQUALS(a, b) {\
+    if (a != b) { \
+        TEST_FAIL("assertion failed %i != %i", a, b); \
+    } \
+};
+
+#define STRING_ASSERT_EQUALS(a, b) { \
+    if (strcmp(a, b) != 0) { \
+        TEST_FAIL("assertion failed \"%s\" != \"%s\"", a, b); \
+    } \
+};
+
+#define TEST(name, ...) { \
+    __label__ __T_STOP__; \
+    const char* __T_NAME__ = #name; \
+    printf(FMT_INFO("running test \"" #name "\"\n")); \
+    __VA_ARGS__ \
+    TEST_PASS("no description provided"); \
+    __T_STOP__:; \
+};
+
+#endif
